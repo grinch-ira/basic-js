d469ce128cb4abcd459c11cba245cba8add8c6f8
Hryniuk Iryna <grinch.ira@gmail.com>
2022-04-26 10:18:19 +0300

    feat: add repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+let result = [];
+let addRes = [];
+let count = options.hasOwnProperty("repeatTimes")?options.repeatTimes : 1;
+let separat = options.hasOwnProperty("separator")?options.separator + "" : "+";
+let additionStr = options.hasOwnProperty("addition")? options.addition + "" : "";
+let additionCount = options.hasOwnProperty("additionRepeatTimes")?options.additionRepeatTimes : 1;
+let additionSeparat = options.hasOwnProperty("additionSeparator")?options.additionSeparator : "|";
+for(let i = 0; i<additionCount; i++){
+  addRes.push(additionStr)
+}
+addRes = addRes.join(additionSeparat);
+for(let i = 0; i<count; i++){
+  result.push(str + addRes)
+}
+return result.join(separat)
+
 }
 
 module.exports = {
